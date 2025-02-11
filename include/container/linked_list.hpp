2e5e5da51c008ca5b98e35d901218cde05148d6e
auntyabby <abbi.codes.ai@gmail.com>
2025-02-11 18:10:21 +0800

    feat: create class for NewsLinkedList

--- include/container/linked_list.hpp
+++ include/container/linked_list.hpp
@@ -2,8 +2,27 @@
 /// Custom Linked List Implementation in C++
 /// </summary>
 #pragma once
+#include "news.hpp"
+using namespace std;
 
-class NewsLinkedList {
-    public:
-    NewsLinkedList();
+struct NewsNodePtr {
+    News data;
+    NewsNodePtr* nextAddress;
+    NewsNodePtr* prevAddress;
 };
+class NewsLinkedList {
+private:
+    NewsNodePtr* head;
+    NewsNodePtr* tail;
+public:
+    NewsLinkedList(string filePath) : head(nullptr), tail(nullptr) {}
+    void InsertNode(News structNews);
+    void insertAtLocation(News structNews, int location);
+    NewsNodePtr* getAtLocation(int location);
+    NewsNodePtr* getHead();
+    NewsNodePtr* getTail();
+    void displayArticle();
+    ~NewsLinkedList();
+};
+
+
